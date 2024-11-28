ed31bb77add9808dcbf6c4e24b3a32821ccad524
yukai0 <65733651+yukai0@users.noreply.github.com>
2024-11-27 21:54:01 -0800

    Revert "changes to App.java, Account.Java, Menu.Java"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,28 +1,7 @@
 // import ...
-import data.*;
-import io.*;
-import model.*;
-import util.*;
-import java.util.*;
-import java.io.IOException;
-
 
 public class App {
-    public static UserDirectory ud = new UserDirectory();
-    public static DataStorage load = new DataStorage(ud);
-    public static Account userAccount;
-
     public static void main(String[] args) {
         // ...
-        try {
-            load.loadData();
-            userAccount = new Account(ud);
-        } catch (IOException | IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-
-
     }
-
 }
